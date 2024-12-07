421e7d6fd8775bfd309d913710c877e78a29c6fc
Dashami1310 <dashami1310@gmail.com>
2024-12-07 11:04:44 +0530

    This is display file

--- aasiya.c
+++ aasiya.c
@@ -1,1 +1,16 @@
-
+void display() 
+{ 
+int i; 
+if(queueempty()) 
+printf("circular queue is empty\n"); 
+else 
+{ 
+for(i=f;i!=r;i=(i+1)%MAX) 
+{ 
+printf("cqueue[%d];",i); 
+printf("%d\n",cqueue[i]); 
+} 
+printf("cqueue[%d]:",i); 
+printf("%d\n",cqueue[i]); 
+} 
+}
