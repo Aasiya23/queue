18e4be761fba67e5ddd697c588c00df6b676a7cd
Madhurivpatil2005 <madhurivp105@gmail.com>
2024-12-07 11:04:04 +0530

    this is insert file

--- aasiya.c
+++ aasiya.c
@@ -1,1 +1,27 @@
-
+void insert(int item) 
+{ 
+if(f==-1) 
+f=0; 
+r=(r+1)%MAX; 
+cqueue[r]=item; 
+} 
+int delete() 
+{ 
+int element; 
+if(queueempty()) 
+{ 
+printf("circular queue is underflow\n");
+return -1;
+}
+else
+{
+element=cqueue[f];
+if(f==r)
+{
+f=-1;r=-1;
+}
+else
+f=(f+1)%MAX;
+return element;
+}
+}
